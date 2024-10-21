37e81dd36180204b531614053c56413687046a20
Carlos Reyes Dueñas <king.slave@gmail.com>
2024-10-21 09:55:57 -0600

    Creación de proyecto
    Se inicializa el proyecto. Se incluye en resaltado de sintaxis básico.

---
+++ Basic SQL Client/Basic SQL Client/Principal.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Basic_SQL_Client
+{
+    partial class Principal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            splitContainer1 = new SplitContainer();
+            editor = new RichTextBox();
+            toolStrip1 = new ToolStrip();
+            toolStripButton1 = new ToolStripButton();
+            splitContainer2 = new SplitContainer();
+            dataGridView1 = new DataGridView();
+            richTextBox2 = new RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer2).BeginInit();
+            splitContainer2.Panel1.SuspendLayout();
+            splitContainer2.Panel2.SuspendLayout();
+            splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(editor);
+            splitContainer1.Panel1.Controls.Add(toolStrip1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(splitContainer2);
+            splitContainer1.Size = new Size(979, 565);
+            splitContainer1.SplitterDistance = 150;
+            splitContainer1.TabIndex = 0;
+            splitContainer1.SplitterMoved += splitContainer1_SplitterMoved;
+            // 
+            // editor
+            // 
+            editor.BorderStyle = BorderStyle.FixedSingle;
+            editor.Dock = DockStyle.Fill;
+            editor.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            editor.Location = new Point(0, 25);
+            editor.Name = "editor";
+            editor.Size = new Size(979, 125);
+            editor.TabIndex = 1;
+            editor.Text = "select * from usuario;";
+            editor.TextChanged += editor_TextChanged;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton1 });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(979, 25);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.Image = Properties.Resources.run;
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(69, 22);
+            toolStripButton1.Text = "Ejecutar";
+            // 
+            // splitContainer2
+            // 
+            splitContainer2.Dock = DockStyle.Fill;
+            splitContainer2.Location = new Point(0, 0);
+            splitContainer2.Name = "splitContainer2";
+            splitContainer2.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            splitContainer2.Panel1.Controls.Add(dataGridView1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            splitContainer2.Panel2.Controls.Add(richTextBox2);
+            splitContainer2.Size = new Size(979, 411);
+            splitContainer2.SplitterDistance = 296;
+            splitContainer2.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.Size = new Size(979, 296);
+            dataGridView1.TabIndex = 0;
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Dock = DockStyle.Fill;
+            richTextBox2.Location = new Point(0, 0);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(979, 111);
+            richTextBox2.TabIndex = 0;
+            richTextBox2.Text = "";
+            // 
+            // Principal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(979, 565);
+            Controls.Add(splitContainer1);
+            Name = "Principal";
+            Text = "Principal";
+            Load += Principal_Load;
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            splitContainer2.Panel1.ResumeLayout(false);
+            splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer2).EndInit();
+            splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private RichTextBox editor;
+        private ToolStrip toolStrip1;
+        private SplitContainer splitContainer2;
+        private RichTextBox richTextBox2;
+        private DataGridView dataGridView1;
+        private ToolStripButton toolStripButton1;
+    }
+}
---
+++ Basic SQL Client/Basic SQL Client/Principal.cs
@@ -0,0 +1,32 @@
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Basic_SQL_Client
+{
+    public partial class Principal : Form
+    {
+        Resaltado r;
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Principal_Load(object sender, EventArgs e)
+        {
+            r = new Resaltado(editor);
+            r.resaltar();
+        }
+
+        private void editor_TextChanged(object sender, EventArgs e)
+        {
+            r.resaltar();
+
+        }
+
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+    }
+}
